88a10e8b7010ce9b09efe0773b26393ea60c4486
kopylovvu <kopylovvu@SQL3TSRV>
2023-03-15 11:38:23 +0300

    ремарки

--- AutoSavePrices/Models/RoutePrice.cs
+++ AutoSavePrices/Models/RoutePrice.cs
@@ -12,10 +12,19 @@ namespace AutoSavePrices.Models
 
         public string IdClient { get; set; }
 
+        /// <summary>
+        /// Полный путь до файла прайсов
+        /// </summary>
         public string FullPath { get; set; }
 
+        /// <summary>
+        /// Путь до папки, в которой лежат прайсы
+        /// </summary>
         public string PathDirectory { get; set; }
 
+        /// <summary>
+        /// Имя файла для отправки на email
+        /// </summary>
         public string NameFile { get; set; }
     }
 }
